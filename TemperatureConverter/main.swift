9ab6df52f32194a49799391c8efe6c5f803e7058
Steven Zhang <YZhang@lcs.on.ca>
2021-02-02 15:31:02 -0500

    Case 1

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Welcome to the temperature Converter")
+print("====================================")
 
+print("This is the Temprature Converter")
+print("Which one you want to convert")
+print("1: Celsius to Fahrenheit.")
+print("2: Celsius tovisce versa.")
+print("Please enter your choise: ")
+let choise = readLine()!
+
+switch choise {
+case "1":
+    print("You have sellect Celsius to Fahrenheit")
+    print("Please enter you number for Celsius")
+    let number = readLine()!
+    
+    
+default:
+    <#code#>
+}
