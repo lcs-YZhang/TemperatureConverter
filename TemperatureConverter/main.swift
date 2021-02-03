362b67c3d014b479c04f1c7bd01a6f44db471376
Steven Zhang <YZhang@lcs.on.ca>
2021-02-02 20:11:16 -0500

    Case 2

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -13,17 +13,34 @@ print("====================================")
 print("This is the Temprature Converter")
 print("Which one you want to convert")
 print("1: Celsius to Fahrenheit.")
-print("2: Celsius tovisce versa.")
+print("2: Fahrenheit to Celsius.")
 print("Please enter your choise: ")
 let choise = readLine()!
+
+func celsiusToFahrenheit (celsius: Double) -> Double {
+    return celsius * 9/5 + 32
+}
+
+func fahrenheittoCelsius (fahrenheit: Double) -> Double {
+    return (fahrenheit - 32) / 9/5
+}
 
 switch choise {
 case "1":
     print("You have sellect Celsius to Fahrenheit")
     print("Please enter you number for Celsius")
-    let number = readLine()!
+    let number = Double(readLine()!)!
     
+    let result = celsiusToFahrenheit(celsius: number)
+    print("It's \(result)°F")
+case "2":
+    print("You have sellect Fahrenheit to Celsius")
+    print("Please enter you number for Fahrenheit")
+    let number = Double(readLine()!)!
+    
+    let result = fahrenheittoCelsius(fahrenheit: number)
+    print("It's \(result)°C")
     
 default:
-    <#code#>
+    print("Please enter again")
 }
